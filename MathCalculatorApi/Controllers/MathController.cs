36ce5c008ec05589bffcd781dfbe42ad4e04883c
Grovre <landonvee111@gmail.com>
2024-02-28 01:09:20 -0600

    first api

---
+++ MathCalculatorApi/Controllers/MathController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MathCalculatorApi.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class MathController : ControllerBase
+{
+    [HttpPost("add")]
+    public ActionResult Add(ICollection<int> nums)
+    {
+        return new JsonResult(new
+        {
+            nums,
+            sum = nums.Sum()
+        });
+    }
+}
